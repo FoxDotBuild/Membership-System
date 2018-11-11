369808c905efea413c6b948f6ab0e92007c43a03
Rick Carlino <rick.carlino@gmail.com>
2018-11-10 21:46:22 -0600

    More typos lol

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,7 +4,7 @@ class Member < ApplicationRecord
   def maybe_set_alias
     return if self.alias.present?
     json = JSON.parse(`curl -H "Authorization:KISI-LOGIN #{API_KEY}" https://api.getkisi.com/members?user_id=#{kisi_actor_id}`)
-    first_n, last_n = json[0].fetch("name")#.split(" ")
-    update_attributes!(name: [first_n, last_n.first].join(" "))
+    first_n, last_n = json[0].fetch("name").split(" ")
+    update_attributes!(alias: [first_n, last_n.first].join(" "))
   end
 end
