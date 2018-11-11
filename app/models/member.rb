02f2bb5e8d86653322b4ba560804ee667fb4001f
Rick Carlino <rick.carlino@gmail.com>
2018-11-10 21:52:26 -0600

    Handle weird data

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,8 +3,10 @@ class Member < ApplicationRecord
 
   def maybe_set_alias
     return if self.alias.present?
-    json = JSON.parse(`curl -H "Authorization:KISI-LOGIN #{API_KEY}" https://api.getkisi.com/members?user_id=#{kisi_actor_id}`)
-    first_n, last_n = json[0].fetch("name").split(" ")
+    json      = JSON.parse(`curl -H "Authorization:KISI-LOGIN #{API_KEY}" https://api.getkisi.com/members?user_id=#{kisi_actor_id}`)
+    full_name = (json || [])[0]["name"]
+    return unless full_name
+    first_n, last_n = full_name.split(" ")
     update_attributes!(alias: [first_n, last_n.first].join(" "))
   end
 end
