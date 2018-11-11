f12a59e2f4f18a7fe4cc297e27d2975fb389afa3
Rick Carlino <rick.carlino@gmail.com>
2018-11-10 21:32:17 -0600

    but can you alias alias?

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ApplicationRecord
   API_KEY = ENV.fetch("kisi_api_token")
 
   def maybe_set_alias
-    return if alias.present?
+    return if self.alias.present?
     json = JSON.parse(`curl -H "Authorization:KISI-LOGIN #{API_KEY}" https://api.getkisi.com/members?user_id=#{id}`)
     first_n, last_n = json.fetch("name").split(" ")
     update_attributes!(name: [first_n, last_n.first].join(" "))
