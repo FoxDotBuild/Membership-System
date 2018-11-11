3c9358d541ffea9e0012b0b16e8b7f84c8dea05c
Rick Carlino <rick.carlino@gmail.com>
2018-11-10 18:03:44 -0600

    Wow symbolize keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def raw_json
-    @raw_json ||= JSON.parse(request.body.read).tap{ |x| symbolize(x) }
+    @raw_json ||= JSON.parse(request.body.read).with_indifferent_access
   rescue JSON::ParserError
     raise OnlyJson
   end
