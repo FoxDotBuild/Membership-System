c7cdf15d719f5e6494bfd6f9df622d5d94cfd636
Rick Carlino <rick.carlino@gmail.com>
2018-11-10 21:29:23 -0600

    Fill in the blanks on missing mmeber info

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,9 @@
 class Member < ApplicationRecord
+  API_KEY = ENV.fetch("kisi_api_token")
+
+  def maybe_set_alias
+    return if alias.present?
+    json = JSON.parse(`curl -H "Authorization:KISI-LOGIN #{API_KEY}" https://api.getkisi.com/members?user_id=#{id}`)
+    update_attributes!(name: json.fetch("name"))
+  end
 end
