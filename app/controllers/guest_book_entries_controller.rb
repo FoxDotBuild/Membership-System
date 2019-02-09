f14dbf0d84424ca72851d9ea460e10ed5d3a9dbe
Rick Carlino <rick.carlino@gmail.com>
2019-02-09 17:23:10 -0600

    Updates to guestbook - Send report to slack

--- app/controllers/guest_book_entries_controller.rb
+++ app/controllers/guest_book_entries_controller.rb
@@ -3,10 +3,19 @@ class GuestBookEntriesController < ApplicationController
   SLACK  = Slack::Notifier.new(ENV.fetch("SLACK_URL", "???"),
                                channel: "#managers",
                                username: "Foxy McFoxFace")
+  REPORT_TPL = [
+    "```",
+    "Name: %{name}",
+    "Email: %{email}",
+    "How did you hear about us?: %{referral}",
+    "Comments (optional): %{misc}",
+    "```",
+  ].join("\n")
 
   def create
     p = params.fetch("guest_book_entry").permit(*FIELDS)
-    GuestBookEntry.create!(p)
+    gbe = GuestBookEntry.create!(p)
+    SLACK.ping(REPORT_TPL % gbe.as_json.deep_symbolize_keys)
     redirect_to action: :new
   end
 
