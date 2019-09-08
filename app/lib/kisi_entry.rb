9c493ca18e50ede6a3e413fcb6df2a48e7606db0
Rick Carlino <rick.carlino@gmail.com>
2019-09-08 15:52:20 -0500

    Possible fix for "invisible" members

--- app/lib/kisi_entry.rb
+++ app/lib/kisi_entry.rb
@@ -1,28 +1,28 @@
 # Take Kisi webhook data (for `unlock` actions) and pipe it into here.
 class KisiEntry < Mutations::Command
-  VISIT               = "You visitied the space"
+  VISIT = "You visitied the space"
   # How much a single visit is worth in XP Points.
   DEFAULT_VISIT_WORTH = 60
-  AWARD_REASON        = AwardReason.find_or_create_by(name: VISIT)
-  THIS_TIMEFRAME      = "created_at > ?"
-  TIME_SLICE          = 6.hours
-  SLACK               = Slack::Notifier.new(ENV.fetch("SLACK_URL", "???"),
-                                            channel: "#who_is_in_the_shop",
-                                            username: "Foxy McFoxFace")
+  AWARD_REASON = AwardReason.find_or_create_by(name: VISIT)
+  THIS_TIMEFRAME = "created_at > ?"
+  TIME_SLICE = 6.hours
+  SLACK = Slack::Notifier.new(ENV.fetch("SLACK_URL", "???"),
+                              channel: "#who_is_in_the_shop",
+                              username: "Foxy McFoxFace")
   # OTHER STUFF WE DONT USE ================================================
   # integer(:id), integer(:object_id), string(:code), string(:created_at),
   # string(:message), array(:references) { interger(:id); string(:type) }
   # ========================================================================
   required do
     integer :actor_id
-    boolean :success,     in: [true]
-    string  :action,      in: ["unlock"]
-    string  :actor_type,  in: ["User"]
-    string  :object_type, in: ["Lock"]
+    boolean :success, in: [true]
+    string :action, in: ["unlock"]
+    string :actor_type, in: ["User"]
+    string :object_type, in: ["Lock"]
   end
 
   def validate
-    too_many = \
+    too_many =
       AwardIssuance.where(member_id: member.id, created_at: time_range).any?
 
     add_error :too_many, :too_many, "too many" if too_many
@@ -30,17 +30,17 @@ class KisiEntry < Mutations::Command
 
   def execute
     maybe_slack_it
-    AwardIssuance.create!(bounty:    DEFAULT_VISIT_WORTH,
+    AwardIssuance.create!(bounty: DEFAULT_VISIT_WORTH,
                           reason_id: AWARD_REASON.id,
-                          member_id: member.id) unless member.alias.downcase.include?("ethan")
+                          member_id: member.id)
   end
 
   private
 
   def maybe_slack_it
     if ENV["SLACK_URL"] && member.alias
-      KisiEntry::SLACK.ping("Ladies and gentlemen, "\
-                            "#{member.alias} HAS ENTERED THE BUILDING :tada:")
+      KisiEntry::SLACK.ping("Ladies and gentlemen, " \
+                            "#{member_alias} HAS ENTERED THE BUILDING :tada:")
     end
   end
 
@@ -48,8 +48,11 @@ class KisiEntry < Mutations::Command
     (Time.now - TIME_SLICE)..Time.now
   end
 
+  def member_alias
+    @member_alias ||= (member.alias || "MYSTERY MEMBER #{member.id}")
+  end
+
   def member
     @member ||= Member.find_or_create_by!(kisi_actor_id: actor_id)
   end
 end
-
--- app/models/member.rb
+++ app/models/member.rb
@@ -11,7 +11,7 @@ class Member < ApplicationRecord
 
   def maybe_set_alias
     return if self.alias.present?
-    json      = JSON.parse(`curl -H "Authorization:KISI-LOGIN #{API_KEY}" https://api.getkisi.com/members?user_id=#{kisi_actor_id}`)
+    json = JSON.parse(`curl -H "Authorization:KISI-LOGIN #{API_KEY}" https://api.getkisi.com/members?user_id=#{kisi_actor_id}`)
     full_name = (json || [])[0]["name"]
     return unless full_name
     first_n, last_n = full_name.split(" ")
